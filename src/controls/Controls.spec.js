da138da91f57257bb3ab4d6121c8c510ccb23b62
Addison-Stavlo <AddieStavlo@gmail.com>
2019-01-30 17:42:14 -0500

    refactored tests to be independent, my coupled tests were giving false positives from how render works in the testing library

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,26 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import "jest-dom/extend-expect";
+
+import Controls from "./Controls";
+
+describe("<Controls />", () => {
+  test("locked button shows opposite state", () => {
+    const locked = render(<Controls locked={true} />).getByTestId("lockbtn");
+    expect(locked).toHaveTextContent(/unlock/i);
+  });
+  test("unlocked button shows opposite state", () => {
+    const unlocked = render(<Controls locked={false} />).getByTestId("lockbtn");
+    expect(unlocked).not.toHaveTextContent(/unlock/i);
+  });
+  test("opened button shows opposite state", () => {
+    const open = render(<Controls closed={false} />).getByTestId("doorbtn");
+    expect(open).toHaveTextContent(/close/i);
+  });
+  test("closed button shows opposite state", () => {
+    const closed = render(<Controls closed={true} />).getByTestId("doorbtn");
+    expect(closed).toHaveTextContent(/open/i);
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -7,46 +7,40 @@ import "jest-dom/extend-expect";
 import Display from "./display";
 
 describe("<Display />", () => {
-  test("displays closed if 'closed' prop is true, open otherwise", () => {
+  test("displays closed if 'closed' prop is true", () => {
     const { getByText } = render(<Display closed={true} />);
-    const checkOpen = render(<Display />).getByText;
-
     getByText(/closed/i);
-    checkOpen(/open/i);
   });
+  test("displays open if 'closed' prop is false", () => {
+    const checkOpen = render(<Display closed={false} />).getByText;
+    //commented out below doesnt work, renders on top of each other, false positives, etc...
+    // const checkClosed = render(<Display closed={true} />).getByText;
 
-  test("displays locked if 'locked' prop is true, unlocked otherwise", () => {
+    checkOpen(/open/i);
+    // checkClosed(/closed/i);
+  });
+  test("displays locked if 'locked' prop is true", () => {
     const checkLocked = render(<Display locked={true} />).getByText;
-    const checkUnlocked = render(<Display />).getByText;
-
     checkLocked(/locked/i);
+  });
+  test("displays unlocked if 'locked' prop is false", () => {
+    const checkUnlocked = render(<Display />).getByText;
     checkUnlocked(/unlocked/i);
   });
-
-  test("when locked or closed use 'red-led' class", () => {
-    //one feature/expected behaviour/assumption per test
-
-    const locked = render(<Display locked={true} closed={false} />).getByTestId(
-      "lock"
-    );
-    const closed = render(<Display locked={false} closed={true} />).getByTestId(
-      "lock"
-    );
-    const lockedAndClosed = render(
-      <Display locked={true} closed={true} />
-    ).getByTestId("lock");
-
-    //as many assertions as needed to prove correctness
+  test("when locked, gate uses red-led class", () => {
+    const locked = render(<Display locked={true} />).getByTestId("lock");
     expect(locked).toHaveClass("red-led");
-    expect(closed).toHaveClass("red-led");
-    expect(lockedAndClosed).toHaveClass("red-led");
   });
-
-  test("when unlocked and open use 'green-led' class", () => {
-    const open = render(<Display locked={false} closed={false} />).getByTestId(
-      "door"
-    );
-
-    expect(open).toHaveClass("green-led");
+  test("when closed, door uses red-led class", () => {
+    const closed = render(<Display closed={true} />).getByTestId("door");
+    expect(closed).toHaveClass("red-led");
+  });
+  test("when unlocked, gate uses green-led class", () => {
+    const unlocked = render(<Display locked={false} />).getByTestId("lock");
+    expect(unlocked).toHaveClass("green-led");
+  });
+  test("when open, door uses green-led class", () => {
+    const opened = render(<Display closed={false} />).getByTestId("door");
+    expect(opened).toHaveClass("green-led");
   });
 });
