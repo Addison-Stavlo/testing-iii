6e68185071768ccbe8f43ffb7c8aab93f92efc08
Addison-Stavlo <AddieStavlo@gmail.com>
2019-01-30 20:16:07 -0500

    added react-mock/state example to dashboard test, inject a specific state to make test more independent

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,17 +3,31 @@ import React from "react";
 import { render, fireEvent } from "react-testing-library";
 import "react-testing-library/cleanup-after-each";
 import "jest-dom/extend-expect";
+import { StateMock } from "@react-mock/state";
 
 import Dashboard from "./Dashboard";
 
 describe("<Dashboard />", () => {
   test("clicking 'close' button changes state", () => {
-    const doorButton = render(<Dashboard />).getByTestId("doorbtn");
+    //assumes initial state false/false
+    //try @react-mock/state
+    //from https://react-testing-examples.com/
+    const renderDashboard = ({ closed }) =>
+      render(
+        <StateMock state={{ closed }}>
+          <Dashboard />
+        </StateMock>
+      );
+    const doorButton = renderDashboard({ closed: false }).getByTestId(
+      "doorbtn"
+    );
+    // const doorButton = render(<Dashboard />).getByTestId("doorbtn");
     expect(doorButton).toHaveTextContent(/close/i);
     fireEvent.click(doorButton);
     expect(doorButton).toHaveTextContent(/open/i);
   });
   test("clicking 'close' on control changes display", () => {
+    //assumes initial state false/false
     const { getByTestId } = render(<Dashboard />);
     const button = getByTestId("doorbtn");
     const display = getByTestId("door");
